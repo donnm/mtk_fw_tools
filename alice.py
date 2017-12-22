73b16a0eb98bbdda5ae4bca0a6bf1cb23cb242fe
Donn Morrison <donn.morrison@idi.ntnu.no>
2017-12-22 02:01:43 +0100

    working encoder up to 20 instructions

--- alice.py
+++ alice.py
@@ -50,11 +50,8 @@ def translate_bl_blx():
 
     print("translated %d bl and %d blx instructions"%(bl_count*2, blx_count*2))
 
-def gen_magic():
-    return
-
 def compress(bitNum, instr):
-#    print("in compress %02x %06x"%(bitNum, instr))
+    print("in compress 0x%02x 0x%08x"%(bitNum, instr))
     global buff, edi12h, ptr
 
     instrAndSeven0ch = instr & 0xff
@@ -63,39 +60,77 @@ def compress(bitNum, instr):
             compress(bitNum - 8, instr >> 8)
             bitNum = 8
 
-        var1 = instrAndSeven0ch & ((1 << bitNum) - 1)
-        var2 = (bitNum - edi12h) - 1
+        var1 = instrAndSeven0ch & ((1 << bitNum) - 1) & 0xff
+        var2 = bitNum - edi12h - 1
         instrAndSeven0ch = var2
 
-        if var2 == 0:
-            buff[ptr] = (buff[ptr] | var1) & 0xff
+        if var2 < 0:
+            # Output
+            print("%d b %02x | %02x -> %02x"%(ptr, buff[ptr], (var1 << -var2) & 0xff, (buff[ptr] | (var1 << -var2) & 0xff) & 0xff))
+            buff[ptr] |= (var1 << -var2) & 0xff
+            edi12h -= bitNum
+        elif var2 == 0:
+            # Output
+            print("%d a %02x | %02x -> %02x"%(ptr, buff[ptr], var1, (buff[ptr] | var1) & 0xff))
+            buff[ptr] |= var1
             ptr += 1
-            buff.append(0x00);
+            buff.append(0x00)
             edi12h = 7
-        elif var2 >> 7:
+        elif var2 > 0:
+            # Output
+            print("%d c %02x | %02x -> %02x"%(ptr, buff[ptr], (var1 >> var2) & 0xff, (buff[ptr] | (var1 >> var2)) & 0xff))
+            buff[ptr] |= (var1 >> var2) & 0xff
+            ptr += 1
+            buff.append(0x00)
+            if (ptr+1) % 0x100 == 0:
+                ptr += 1
+                buff.append(0x00)
+                var2 = instrAndSeven0ch
+            # Output
+            print("%d d %02x | %02x -> %02x"%(ptr, buff[ptr], var1  << (8 - var2 & 0xff) & 0xff, (buff[ptr] | var1  << (8 - var2 & 0xff) & 0xff) & 0xff))
+            buff[ptr] |= var1  << ((8 - var2) & 0xff) & 0xff
+            edi12h = 0x07 - var2
+'''
+        if var2 == 0:
+            # Output
+            print("%d a %02x | %02x -> %02x"%(ptr, buff[ptr], var1 & 0xff, (buff[ptr] | var1) & 0xff))
+            buff[ptr] |= var1 & 0xff
+            ptr += 1
+            buff.append(0x00)
+            edi12h = 7
+        elif (0xff & var2) >> 7:
             var3 = var2
             if (0xff & var2) >> 7:
                 var3 = var2 | 0xFFFFFF00
             var3 = -var3 & 0xffffffff
             var1 = var1 << var3
-            buff[ptr] = (buff[ptr] | var1) & 0xff
+            # Output
+            print("%d b %02x | %02x -> %02x"%(ptr, buff[ptr], var1 & 0xff, (buff[ptr] | var1) & 0xff))
+            buff[ptr] |= var1 & 0xff
             edi12h -= bitNum
         else:
             var5 = var1 >> var2
-            buff[ptr] = (buff[ptr] | var5) & 0xff
+            # Output
+            print("%d c %02x | %02x -> %02x"%(ptr, buff[ptr], var5 & 0xff, (buff[ptr] | var5) & 0xff))
+            buff[ptr] |= var5 & 0xff
             ptr += 1
-            buff.append(0x00);
-            if ptr == 0x100:
-                var2 = instrAndSeven0ch
+            buff.append(0x00)
+#            if ptr % 0x100 == 0 and ptr > 0:
+#                ptr += 1
+#                buff.append(0x00)
+#                var2 = instrAndSeven0ch
             var3 = var2
             if (0xff & var2) >> 7:
                 var3 = var2 | 0xFFFFFF00
             var4 = 8 - var3
             var1 = var1 << var4
             var4 = 0x07
-            buff[ptr] = (buff[ptr] | var1) & 0xff
+            # Output
+            print("%d d %02x | %02x -> %02x"%(ptr, buff[ptr], var1 & 0xff, (buff[ptr] | var1) & 0xff))
+            buff[ptr] |= var1 & 0xff
             var4 = var4 - var2
             edi12h = var4 & 0xff
+'''
 
 edi12h = 0x07
 #buff = bytearray([0x00])
@@ -111,6 +146,10 @@ f.close()
 
 translate_bl_blx()
 
+f = open("translated-py.bin", "wb")
+f.write(buff)
+f.close()
+
 # Generate histogram
 # Need to make sure it is sorted in the same way ALICE.exe sorts,
 # that being that first by frequency, then for instructions of the
@@ -118,6 +157,9 @@ translate_bl_blx()
 # in ALICE.bin, or something else?
 # Most likely ALICE.exe gets the order from the way the BST
 # is traversed.
+
+# Construct fake ALICE.bin with a desired histogram and try to match
+# the output.
 
 instrs=[bytes(buff[i*2:i*2+2]) for i in range(int(len(buff)/2))]
 hist=Counter(instrs)
@@ -128,45 +170,78 @@ shist=[instr for instr,freq in shist]
 # hist.get(b'\x00(')
 # sorted(hist, key=hist.get, reverse=True)
 
-
-f = open("translated-py.bin", "wb")
-f.write(buff)
-f.close()
-
-# Generate magic vector, funked instructions
+# Generate magic vector, funked instructions dictionary (before_encode.bin)
 range_regs = [0x0, 0x10, 0x50, 0xd0, 0x1d0, 0x3d0, 0xbd0, 0x1bd0]
 codes = [0x07, 0x09, 0x0A, 0x0B, 0x0C, 0x0E, 0x0F, 0x13]
 starts = [0x0, 0x40, 0x100, 0x300, 0x800, 0x2800, 0x6000, 0x70000]
 
 magic = bytearray(0x10000)
+fshist = dict()
+
 for r in range(len(range_regs)-1):
     instrnr = 0
     for i in range(range_regs[r], range_regs[r+1]):
-        print("0x%02x 0x%04x 0x%08x %d"%(codes[r], struct.unpack("<H", shist[i])[0], (instrnr | starts[r]), shist_f[i]))
+        instr_idx = struct.unpack("<H", shist[i])[0]
+        print("0x%02x 0x%04x 0x%08x %d %d"%(codes[r], instr_idx, (instrnr | starts[r]), shist_f[i], i))
         # Magic
-        magic[ struct.unpack("<H", shist[i])[0] ] = codes[r]
+        magic[ instr_idx ] = codes[r]
         # Instr
+        fshist[ instr_idx ] = instrnr | starts[r]
         instrnr += 1
 
 offset = len(range_regs)-1
 for i in range(range_regs[offset], len(hist)):
-    print("0x%02x 0x%04x 0x%08x %d"%(codes[offset], struct.unpack("<H", shist[i])[0], (struct.unpack("<H", shist[i])[0] | starts[offset]), shist_f[i]))
+    instr_idx = struct.unpack("<H", shist[i])[0]
+    print("0x%02x 0x%04x 0x%08x %d %d"%(codes[offset], instr_idx, (instr_idx | starts[offset]), shist_f[i], i))
     # Magic
-    magic[ struct.unpack("<H", shist[i])[0] ] = codes[offset]
-    # Instr
+    magic[ instr_idx ] = codes[offset]
+    fshist[ instr_idx ] = instr_idx | starts[offset]
 
 f=open("magic-py.bin", "wb")
 f.write(magic)
 f.close()
 
-sys.exit()
+f=open("before_encode-py.bin", "wb")
+for l,v in fshist.items():
+    f.seek(l*4)
+    f.write(struct.pack("<L", v))
 
-with open(sys.argv[1], "rb") as f:
-    data = f.read(2)
-    while data:
-        instr = int.from_bytes(data, "little")
-        compress(0x13, 0x70000 | instr)
-        data = f.read(2)
+f.close()
 
-for b in buff:
-    print("%02x " % b)
+
+# Try to encode!
+
+if len(sys.argv) == 4:
+    f=open(sys.argv[2], "rb")
+    magic=bytearray(f.read())
+    f.close()
+
+    fshist=dict()
+    f=open(sys.argv[3], "rb")
+    for instr_idx in range(0x10000):
+        instr = struct.unpack("<L", f.read(4))[0]
+        fshist[ instr_idx ] = instr
+    f.close()
+
+ptr = 0
+buff = bytearray([0x00])
+count = 0
+for instr in instrs:
+    instr_idx = struct.unpack("<H", instr)[0]
+    print("instr_idx %d 0x%04x total %d"%(instr_idx, instr_idx, len(fshist)))
+    bitnum = magic[ instr_idx ]
+    finstr = fshist [ instr_idx ]
+    print("calling compress 0x%02x 0x%08x"%(bitnum, finstr))
+    compress(bitnum, finstr)
+
+    count += 1
+    if count % 0x20 == 0 and count > 0:
+        ptr += 1
+        buff.append(0x00)
+        # Do some stuff here...
+        print("encoded count %04x at blocksize 0x20"%(count))
+
+f=open("alice-py", "wb")
+len = f.write(buff[0:ptr])
+print("wrote alice-py %d bytes"%len)
+f.close()
