1fab577b952addb83b1bdb60a514fa7dcab19498
Donn Morrison <donn.morrison@idi.ntnu.no>
2017-12-31 01:14:21 +0100

    unpack/decode

--- alice.py
+++ alice.py
@@ -2,9 +2,27 @@ import sys
 import struct
 from collections import Counter
 
-# magic.bin          - dictionary length array of bitNums indexed by instruction
+'''
+ALICE.exe encoder
+    1. Read instructions from ALICE.bin
+    2. Translate BL/BLX
+    3. Generate dictionary
+    4. Generate magic.bin bitnums, before_encoded.bin encoded instructions
+    5. Compress - iterate through translated instructions (as index), encode
+    6. Mapping table comes from bitpack() and blocksize, low byte unknown
+    7. Postprocess
+
+# magic.bin          - dictionary length array of lengths indexed by translated instruction
 # before_encoded.bin - ranged instructions (32-bit form) in dictionary indexed by
-#                      frequency
+#                      translated instruction
+#
+# TODO
+# - DoMagic13And70000 sorting based on BST?
+# - bitpack() end bytes (0x02) and padding
+#
+#
+'''
+
 
 def translate_bl_blx():
     global buff
@@ -50,48 +68,50 @@ def translate_bl_blx():
 
     print("translated %d bl and %d blx instructions"%(bl_count*2, blx_count*2))
 
-def compress(bitNum, instr):
-    print("in compress 0x%02x 0x%08x"%(bitNum, instr))
-    global buff, edi12h, ptr, count
+def bitpack(length, instr):
+    print("in bitpack 0x%02x 0x%08x"%(length, instr))
+    global buff, length_remain, ptr, count
 
-    instrAndSeven0ch = instr & 0xff
-    if bitNum != 0:
-        if bitNum > 8:
-            compress(bitNum - 8, instr >> 8)
-            bitNum = 8
+    instr_part = instr & 0xff
+    if length != 0:
+        if length > 8:
+            bitpack(length - 8, instr >> 8)
+            length = 8
 
-        var1 = instrAndSeven0ch & ((1 << bitNum) - 1) & 0xff
-        var2 = bitNum - edi12h - 1
-        instrAndSeven0ch = var2
+        length_part = length - length_remain - 1
+        print("length_part: %d = %d - %d - 1"%(length_part, length, length_remain))
 
-        if var2 < 0:
-            # Output
-            print("%d b %02x | %02x -> %02x"%(ptr, buff[ptr], (var1 << -var2) & 0xff, (buff[ptr] | (var1 << -var2) & 0xff) & 0xff))
-            buff[ptr] |= (var1 << -var2) & 0xff
-            edi12h -= bitNum
-        elif var2 == 0:
-            # Output
-            print("%d a %02x | %02x -> %02x"%(ptr, buff[ptr], var1, (buff[ptr] | var1) & 0xff))
-            buff[ptr] |= var1
+        if length_part < 0:
+            # shift left
+            print("shifting 0x%02x << %d = 0x%02x"%(instr_part, -length_part, (instr_part << -length_part) & 0xff))
+            print("%d b %02x | %02x -> %02x"%(ptr, buff[ptr], (instr_part << -length_part) & 0xff, (buff[ptr] | (instr_part << -length_part) & 0xff) & 0xff))
+            buff[ptr] |= (instr_part << -length_part) & 0xff
+            print("length_remain: %d = %d - %d"%(length_remain - length, length_remain, length))
+            length_remain -= length
+        elif length_part == 0:
+            # no shift
+            print("no shift 0x%02x"%(instr_part))
+            print("%d a %02x | %02x -> %02x"%(ptr, buff[ptr], instr_part, (buff[ptr] | instr_part) & 0xff))
+            buff[ptr] |= instr_part
             ptr += 1
             buff.append(0x00)
-            print("compress 1st buff.append")
-            edi12h = 7
-        elif var2 > 0:
-            # Output
-            print("%d c %02x | %02x -> %02x"%(ptr, buff[ptr], (var1 >> var2) & 0xff, (buff[ptr] | (var1 >> var2)) & 0xff))
-            buff[ptr] |= (var1 >> var2) & 0xff
+            print("bitpack 1st buff.append")
+            length_remain = 7
+            print("length_remain: 7")
+        elif length_part > 0:
+            # shift right
+            print("shifting 0x%02x >> %d = 0x%02x"%(instr_part, length_part, (instr_part >> length_part) & 0xff))
+            print("%d c %02x | %02x -> %02x"%(ptr, buff[ptr], (instr_part >> length_part) & 0xff, (buff[ptr] | (instr_part >> length_part)) & 0xff))
+            buff[ptr] |= (instr_part >> length_part) & 0xff
             ptr += 1
             buff.append(0x00)
-            print("compress 2nd buff.append")
-            # Output
-            print("%d d %02x | %02x -> %02x"%(ptr, buff[ptr], var1  << (8 - var2 & 0xff) & 0xff, (buff[ptr] | var1  << (8 - var2 & 0xff) & 0xff) & 0xff))
-            buff[ptr] |= var1  << ((8 - var2) & 0xff) & 0xff
-            edi12h = 0x07 - var2
-
-edi12h = 0x07
-#buff = bytearray([0x00])
-ptr = 0
+            print("bitpack 2nd buff.append")
+            # shift left into new byte
+            print("shifting 0x%02x << %d = 0x%02x"%(instr_part, 8 - length_part, instr_part  << ((8 - length_part) & 0xff) & 0xff))
+            print("%d d %02x | %02x -> %02x"%(ptr, buff[ptr], instr_part  << (8 - length_part & 0xff) & 0xff, (buff[ptr] | instr_part  << (8 - length_part & 0xff) & 0xff) & 0xff))
+            buff[ptr] |= instr_part  << ((8 - length_part) & 0xff) & 0xff
+            length_remain = 7 - length_part
+            print("length_remain: %d = 7 - %d"%(length_remain, length_part))
 
 # Read ALICE.bin
 
@@ -100,7 +120,7 @@ buff = bytearray(f.read())
 f.close()
 
 # Translate BL and BLX instructions
-
+ptr = 0
 translate_bl_blx()
 
 f = open("translated-py.bin", "wb")
@@ -117,18 +137,16 @@ f.close()
 
 # Construct fake ALICE.bin with a desired histogram and try to match
 # the output.
-
 instrs=[bytes(buff[i*2:i*2+2]) for i in range(int(len(buff)/2))]
+'''
 hist=Counter(instrs)
 shist=sorted(hist.items(), key=lambda x: (-x[1], x[0]))
 shist_f=[freq for instr,freq in shist]
 shist=[instr for instr,freq in shist]
 
-# hist.get(b'\x00(')
-# sorted(hist, key=hist.get, reverse=True)
-
 # Generate magic vector, funked instructions dictionary (before_encode.bin)
 range_regs = [0x0, 0x10, 0x50, 0xd0, 0x1d0, 0x3d0, 0xbd0, 0x1bd0]
+#range_regs = [0x0, 0x02, 0x06, 0x0a, 0x0e, 0x12, 0x16, 0x1e]
 codes = [0x07, 0x09, 0x0A, 0x0B, 0x0C, 0x0E, 0x0F, 0x13]
 starts = [0x0, 0x40, 0x100, 0x300, 0x800, 0x2800, 0x6000, 0x70000]
 
@@ -164,7 +182,7 @@ for l,v in fshist.items():
     f.write(struct.pack("<L", v))
 
 f.close()
-
+'''
 
 # Try to encode!
 
@@ -181,6 +199,7 @@ if len(sys.argv) == 4:
     f.close()
 
 ptr = 0
+length_remain = 0x07
 buff = bytearray([0x00])
 count = 0
 for instr in instrs:
@@ -188,19 +207,23 @@ for instr in instrs:
     print("instr_idx %d 0x%04x total %d"%(instr_idx, instr_idx, len(fshist)))
     bitnum = magic[ instr_idx ]
     finstr = fshist [ instr_idx ]
-    print("calling compress 0x%02x 0x%08x"%(bitnum, finstr))
-    compress(bitnum, finstr)
+    print("calling bitpack 0x%02x 0x%08x"%(bitnum, finstr))
+    bitpack(bitnum, finstr)
 
     count += 1
-    if count % 0x20 == 0 and count > 0 and edi12h != 0x07:
-        edi12h = 0x07
-        ptr += 1
-        buff.append(0x00)
-        print("buff.append")
-        # Do some stuff here...
-        print("encoded count %04x at blocksize 0x20"%(count))
+    if count % 0x20 == 0:
+        if count % 0x40 == 0:
+            print("hit instruction blocksize 0x40 at ptr 0x%08x"%(ptr))
+        if count > 0 and length_remain != 0x07:
+            length_remain = 0x07
+            ptr += 1
+            buff.append(0x00)
+            print("buff.append")
+            # Do some stuff here...
+
+# TODO Need to add some padding here
 
 f=open("alice-py", "wb")
-len = f.write(buff[0:ptr])
+len = f.write(buff)
 print("wrote alice-py %d bytes"%len)
 f.close()
